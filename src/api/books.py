123cc66f12d554f847e1800f1f88d7f25fa7572e
Victor André <victorandre957@gmail.com>
2023-11-19 19:14:08 -0300

    cria funções de api para livros

--- src/api/books.py
+++ src/api/books.py
@@ -19,13 +19,24 @@ def bd_connect():
     except Exception as e:
         print(f'Error connecting to the database: {e}')
 
-
-def create():
+def create_book(book):
     try:
         conn = bd_connect()
         cur = conn.cursor()
         cur.execute(
-            "INSERT INTO Books"
+            "INSERT INTO Books (isbn, title, author, description, category, date_acquisition, conservation_status, physical_location, book_cover_url, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (
+                book["isbn"], 
+                book["title"], 
+                book["author"], 
+                book["description"],
+                book["category"], 
+                book["date_acquisition"], 
+                book["conservation_status"], 
+                book["physical_location"], 
+                book["book_cover_url"], 
+                book["status"], 
+            )
         )
         conn.commit()
         conn.close()
@@ -33,8 +44,49 @@ def create():
     except Exception as e:
         print(f'Error inserting book: {e}')
         return False
+    
 
-def get():
+def update_book(book):
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute(
+            "UPDATE Books SET title = %s, author = %s, description = %s, category = %s, date_acquisition = %s, conservation_status = %s, physical_location = %s, book_cover_url = %s, status = %s WHERE isbn = %s",
+            (
+                book['title'], 
+                book['author'], 
+                book['description'],
+                book['category'], 
+                book['date_acquisition'], 
+                book['conservation_status'], 
+                book['physical_location'], 
+                book['book_cover_url'], 
+                book['status'], 
+                book['isbn'])
+        )
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f'Error updating book: {e}')
+        return False
+
+def delete_book(isbn):
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute(
+            "DELETE FROM Books WHERE isbn = %s",
+            (isbn)
+        )
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f'Error deleting book: {e}')
+        return False
+
+def get_book():
     try:
         conn = bd_connect()
         cur = conn.cursor()
@@ -44,3 +96,25 @@ def get():
         return books
     except Exception as e:
         print(f'Error fetching books: {e}')
+
+def get_book_by_isbn(isbn):
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Books WHERE isbn = %s', (isbn))
+        books = cur.fetchall()
+        conn.close()
+        return books
+    except Exception as e:
+        print(f'Error fetching books: {e}')
+
+def get_available_books():
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Books WHERE status = 'AVAILABLE'")
+        available_books = cur.fetchall()
+        conn.close()
+        return available_books
+    except Exception as e:
+        print(f'Error fetching available books: {e}')
