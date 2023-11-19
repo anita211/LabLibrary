8815ab7929dbb318d7cfcb62008e62a00bf62261
Victor André <victorandre957@gmail.com>
2023-11-19 19:14:58 -0300

    cria funções de api para emprestimos

--- src/api/loan.py
+++ src/api/loan.py
@@ -19,29 +19,78 @@ def bd_connect():
     except Exception as e:
         print(f'Error connecting to the database: {e}')
 
-# TODO: Atualizar as funções exemplo para funcionarem com o banco correto
-def create():
+def create_loan(loan_data):
     try:
         conn = bd_connect()
-
         cur = conn.cursor()
         cur.execute(
-            "INSERT INTO Loan"
+            "INSERT INTO Loan (loan_date, expected_return_date, status, id_book, id_material, id_user) VALUES (%s, %s, %s, %s, %s, %s)",
+            (
+                loan_data["loan_date"],
+                loan_data["expected_return_date"],
+                loan_data.get("status", "IN_PROGRESS"),
+                loan_data.get("id_book"),
+                loan_data.get("id_material"),
+                loan_data["id_user"]
+            )
         )
         conn.commit()
         conn.close()
         return True
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Error creating loan: {e}')
         return False
 
-def get():
+def update_loan_status(loan_id, new_status):
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute(
+            "UPDATE Loan SET status = %s WHERE id = %s",
+            (new_status, loan_id)
+        )
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f'Error updating loan status: {e}')
+        return False
+
+def delete_loan(loan_id):
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute(
+            "DELETE FROM Loan WHERE id = %s",
+            (loan_id,)
+        )
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f'Error deleting loan: {e}')
+        return False
+
+def get_all_loans():
     try:
         conn = bd_connect()
         cur = conn.cursor()
         cur.execute('SELECT * FROM Loan')
-        loan = cur.fetchall()
+        loans = cur.fetchall()
         conn.close()
-        return loan
+        return loans
     except Exception as e:
-        print(f'Error fetching loan: {e}')
+        print(f'Error fetching loans: {e}')
+        return None
+    
+def get_in_progress_loans():
+    try:
+        conn = bd_connect()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM Loan WHERE status = 'IN_PROGRESS'")
+        in_progress_loans = cur.fetchall()
+        conn.close()
+        return in_progress_loans
+    except Exception as e:
+        print(f'Error fetching in-progress loans: {e}')
+        return None
