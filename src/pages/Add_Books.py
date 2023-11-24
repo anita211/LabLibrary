6bc2d431cd66b07dfaa453ac231b4cd9d5dd1c8a
Victor André <victorandre957@gmail.com>
2023-11-24 10:50:27 -0300

    usa as novas funções de api em pequenos testes

--- src/pages/Add_Books.py
+++ src/pages/Add_Books.py
@@ -1,21 +1,22 @@
 import streamlit as st
-from api.books import create as books_create
+from api.books import Book
+
 
 st.header('Add a New Book')
 
-isbn = st.text_input('isbn')
+isbn = st.number_input('isbn', min_value=0)
 title = st.text_input('Title')
 author = st.text_input('Author')
 description = st.text_input('description')
 category = st.selectbox("category", ['ROMANCE', 'COMEDY', 'DRAMA', 'SCIFI', 'HORROR'])
-date_acquisition = st.number_input('Date Acquisition', min_value=0)
+date_acquisition = st.date_input('Date Acquisition')
 conservation_status = st.selectbox('Conservation Status', ['NEW', 'AVERAGE', 'OLD', 'DAMAGED'])
 physical_location = st.text_input('Physical Location')
 book_cover_url = st.text_input('Book Cover url')
 status = st.selectbox("Status", ['AVAILABLE', 'BORROWED'])
 
 if st.button('Save'):
-    if books_create(
+    new_book = Book(
         int(isbn), 
         title, 
         author, 
@@ -26,7 +27,8 @@ if st.button('Save'):
         physical_location,
         book_cover_url,
         status,
-    ):
+    )
+    if new_book.create_book():
         st.success('Book inserted successfully!')
     else:
         st.error('Error inserting book.')
--- src/pages/List_Books.py
+++ src/pages/List_Books.py
@@ -1,8 +1,23 @@
 import streamlit as st
-from api.books import get as books_list
+from api.books import Book
+
+st.set_page_config(page_title="List of Books",layout='wide')
 
 st.header('List of Books')
-books = books_list()
+books = Book().get_books()
+
 if books:
     for book in books:
-        st.write(f"ID: {book[0]}, Title: {book[1]}, Author: {book[2]}, Description: {book[3]}")
+        st.write(f'## {book.title}')
+        st.text(f'ISBN: {book.isbn}')
+        st.text(f'Author: {book.author}')
+        st.text(f'Description: {book.description}')
+        st.text(f'Category: {book.category}')
+        st.text(f'Date Acquisition: {book.date_acquisition}')
+        st.text(f'Conservation Status: {book.conservation_status}')
+        st.text(f'Physical Location: {book.physical_location}')
+        st.text(f'Book Cover url: {book.book_cover_url}')
+        st.text(f'Status: {book.status}')
+        
+        st.text('')
+        st.text('')
