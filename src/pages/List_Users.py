c7b36b818eb4f691573303764557416de58db820
Thiago <thiagocrrabelo@gmail.com>
2023-12-09 17:53:39 -0300

    Page List_Users: add search bar

--- src/pages/List_Users.py
+++ src/pages/List_Users.py
@@ -41,23 +41,30 @@ USER_NO_IMG = f'''
 # Page
 
 PAGE_TITLE = "List of Users"
-
 st.set_page_config(page_title=PAGE_TITLE, layout='wide')
-
 st.header(PAGE_TITLE)
+search_term = st.text_input("Search by Username, First or Last name or Role")
+
 users = User().get_users()
 
 if users:
   for index, user in enumerate(users):
-    st.markdown(
-      f'<div style="{(DIV_MAIN)}">'
-        f'<div style="{DIV_CONT}">'
-          f'<h2 style="{DIV_ELEM}">{user.username} - {user.first_name} {user.last_name}</h2>'
-          f'<h3 style="{DIV_ELEM}">{user.role}</h3>'
-        f'</div>'
-      f'<img src="{user.user_photo_url}" alt="User Photo" style="{USER_IMG if user.user_photo_url is not None else USER_NO_IMG}">'
-      f'</div>',
-      unsafe_allow_html=True
-    )
+    if (
+      not search_term
+      or search_term.lower() in user.username.lower()
+      or search_term.lower() in user.first_name.lower()
+      or search_term.lower() in user.last_name.lower()
+      or search_term.lower() in user.role.lower()
+    ):
+      st.markdown(
+        f'<div style="{(DIV_MAIN)}">'
+          f'<div style="{DIV_CONT}">'
+            f'<h2 style="{DIV_ELEM}">{user.username} - {user.first_name} {user.last_name}</h2>'
+            f'<h3 style="{DIV_ELEM}">{user.role}</h3>'
+          f'</div>'
+        f'<img src="{user.user_photo_url}" alt="User Photo" style="{USER_IMG if user.user_photo_url is not None else USER_NO_IMG}">'
+        f'</div>',
+        unsafe_allow_html=True
+      )
 
-    st.text('')
+      st.text('')
