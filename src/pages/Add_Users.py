2de809e5551b049ba34cf4935d88ac2ca36364c6
Thiago <thiagocrrabelo@gmail.com>
2023-12-09 17:54:05 -0300

    Page Add_Users: refactor

--- src/pages/Add_Users.py
+++ src/pages/Add_Users.py
@@ -1,13 +1,15 @@
 import streamlit as st
 from api.users import User
 
+ROLES = ['MEMBER', 'ADMIN']
+
 st.header('Add a New User')
 
 username = st.text_input('Username')
 first_name = st.text_input('First name')
 last_name = st.text_input('Last name')
 user_photo_url = st.text_input('User photo url')
-role = st.selectbox('Role', ['MEMBER', 'ADMIN'])
+role = st.selectbox('Role', ROLES)
 password = st.text_input('Password', type='password')
 
 if st.button('Save'):
@@ -15,7 +17,7 @@ if st.button('Save'):
     username=username,
     first_name=first_name,
     last_name=last_name,
-    user_photo_url=user_photo_url,
+    user_photo_url=user_photo_url if user_photo_url != '' else None,
     role=role,
     password=password
   )
