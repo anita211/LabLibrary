00bfc95aa3062b59de984624366511b0fe1fcec1
Victor André <victorandre957@gmail.com>
2023-12-12 18:08:21 -0300

    Add_Loan  show user name in select

--- src/pages/Add_Loan.py
+++ src/pages/Add_Loan.py
@@ -15,8 +15,14 @@ TeachingMaterialsId = [tm.id for tm in TeachingMaterial().get_available_teaching
 TeachingMaterials = TeachingMaterial().get_available_teaching_materials()
 ObjectType = st.selectbox('Object Type', LoanTypes)
 
+if logged_user["role"] == "ADMIN":
+    users_list = User().get_users()
+    users_names = [f"{user.first_name} {user.last_name}" for user in users_list]
+
+
 # Diferencia os campos de Livro e Material Didático
 if ObjectType == 'BOOK':
+
     st.write('Book')
     book_id = st.selectbox('Book ISBN', BooksId)
     if BooksId != []:
@@ -29,21 +35,19 @@ else:
 
 # Diferencia os campos dependedo do tipo de usuário
 loan_date = st.date_input('Loan Date')
+
 if logged_user["role"] == "MEMBER":
     loan_status='IN_PROGRESS'
     id_user = logged_user["id"]
     expected_return_date = datetime.date.today() + datetime.timedelta(days=7) # default - 7 dias a partir da data atual
+
 elif logged_user["role"] == "ADMIN":
-    status = st.selectbox('Status', ['BORROWED', 'RETURNED'])
-    id_user = st.selectbox('User', [user.id for user in User().get_users()])
+
+    id_user = st.selectbox('User', [user.id for user in users_list], format_func=lambda x: users_names[x - 1])
     st.write('Name - ' + User(id_user).get_user_by_id().first_name)
-    if status == 'BORROWED':
-        default_date = datetime.date.today() + datetime.timedelta(days=7)
-        expected_return_date = st.date_input('Expected Return Date', default_date)
-        loan_status='IN_PROGRESS'
-    else: 
-        expected_return_date = st.date_input('Return Date')
-        loan_status='COMPLETED'
+    default_date = datetime.date.today() + datetime.timedelta(days=7)
+    expected_return_date = st.date_input('Expected Return Date', default_date)
+    loan_status='IN_PROGRESS'
 
 
 if st.button('Confirm'):
