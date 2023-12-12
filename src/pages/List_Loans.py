7177c7a6dadf2553145c038027483a30587046de
caefleury <caefleury15@gmail.com>
2023-12-12 13:28:22 -0300

    loans index role fixes

--- src/pages/List_Loans.py
+++ src/pages/List_Loans.py
@@ -3,7 +3,11 @@ from api.users import User
 from api.loan import Loan
 from api.books import Book
 from api.teaching_materials import TeachingMaterial
+from globals import logged_user
 
+
+user_role = logged_user["role"]
+user_id = logged_user["id"]
 loans = Loan().get_all_loans()
 
 def get_loan_color(loan_index):
@@ -11,7 +15,6 @@ def get_loan_color(loan_index):
     return colors[loan_index % len(colors)]
 
 st.header('List Loans')
-
 
 search_term = st.text_input("Search by id, description, category, date acquisition, serie number, etc.")
 if loans:
@@ -30,42 +33,82 @@ if loans:
             or search_term.lower() in book_name.lower()
         ):
             background_color = get_loan_color(index)
-            if loan.id_book is None:
-                st.markdown(
-                    f'<div style="display: flex; align-items: center; padding: 10px; border-radius: 5px; background-color: {background_color}; color: black">'
-                        f'<div style="flex: 1; padding-right: 10px;">'
-                            f'<h2 style="color: black">ID: {loan.id}</h2>'
-                            f'<div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-left: 20px;">'
-                                f'<p>Status: {loan.status}</p>'
-                                f'<p>Loan Date: {loan.loan_date}</p>'
-                                f'<p>Expected Return Date: {loan.expected_return_date}</p>'
-                                f'<p>Teaching Material ID: {loan.id_material}</p>'
-                                f'<p>Library User ID: {loan.id_user}</p>'
-                                f'<p>Library User First Name: {loanee_first_name}</p>'
+            if user_role == "ADMIN":
+                if loan.id_book is None: # Material Didático
+                    st.markdown(
+                        f'<div style="display: flex; align-items: center; padding: 10px; border-radius: 5px; background-color: {background_color}; color: black">'
+                            f'<div style="flex: 1; padding-right: 10px;">'
+                                f'<h2 style="color: black">ID: {loan.id}</h2>'
+                                f'<div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-left: 20px;">'
+                                    f'<p>Status: {loan.status}</p>'
+                                    f'<p>Loan Date: {loan.loan_date}</p>'
+                                    f'<p>Expected Return Date: {loan.expected_return_date}</p>'
+                                    f'<p>Teaching Material ID: {loan.id_material}</p>'
+                                    f'<p>Library User ID: {loan.id_user}</p>'
+                                    f'<p>Library User First Name: {loanee_first_name}</p>'
+                                f'</div>'
                             f'</div>'
-                        f'</div>'
-                    f'</div>',
-                    unsafe_allow_html=True
-                )
-                st.text('')
-            else:
-                book_name = Book(loan.id_book).get_book_by_isbn().title
-                st.markdown(
-                    f'<div style="display: flex; align-items: center; padding: 10px; border-radius: 5px; background-color: {background_color}; color: black">'
-                        f'<div style="flex: 1; padding-right: 10px;">'
-                            f'<h2 style="color: black">ID: {loan.id}</h2>'
-                            f'<div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-left: 20px;">'
-                                f'<p>Status: {loan.status}</p>'
-                                f'<p>Loan Date: {loan.loan_date}</p>'
-                                f'<p>Expected Return Date: {loan.expected_return_date}</p>'
-                                f'<p>Book ID: {loan.id_book}</p>'
-                                f'<p>Book Name: {book_name}</p>'
-                                f'<p>Library User ID: {loan.id_user}</p>'
-                                f'<p>Library User First Name: {loanee_first_name}</p>'
+                        f'</div>',
+                        unsafe_allow_html=True
+                    )
+                    st.text('')
+                else: # Livro
+                    book_name = Book(loan.id_book).get_book_by_isbn().title
+                    st.markdown(
+                        f'<div style="display: flex; align-items: center; padding: 10px; border-radius: 5px; background-color: {background_color}; color: black">'
+                            f'<div style="flex: 1; padding-right: 10px;">'
+                                f'<h2 style="color: black">ID: {loan.id}</h2>'
+                                f'<div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-left: 20px;">'
+                                    f'<p>Status: {loan.status}</p>'
+                                    f'<p>Loan Date: {loan.loan_date}</p>'
+                                    f'<p>Expected Return Date: {loan.expected_return_date}</p>'
+                                    f'<p>Book ID: {loan.id_book}</p>'
+                                    f'<p>Book Name: {book_name}</p>'
+                                    f'<p>Library User ID: {loan.id_user}</p>'
+                                    f'<p>Library User First Name: {loanee_first_name}</p>'
+                                f'</div>'
                             f'</div>'
-                        f'</div>'
-                    f'</div>',
-                    unsafe_allow_html=True
-                )
-                st.text('')
+                        f'</div>',
+                        unsafe_allow_html=True
+                    )
+                    st.text('')
+            elif user_role == "MEMBER" and user_id == loan.id_user:
+                if loan.id_book is None: # Material Didático
+                    st.markdown(
+                        f'<div style="display: flex; align-items: center; padding: 10px; border-radius: 5px; background-color: {background_color}; color: black">'
+                            f'<div style="flex: 1; padding-right: 10px;">'
+                                f'<h2 style="color: black">ID: {loan.id}</h2>'
+                                f'<div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-left: 20px;">'
+                                    f'<p>Status: {loan.status}</p>'
+                                    f'<p>Loan Date: {loan.loan_date}</p>'
+                                    f'<p>Expected Return Date: {loan.expected_return_date}</p>'
+                                    f'<p>Teaching Material ID: {loan.id_material}</p>'
+                                    f'<p>Library User ID: {loan.id_user}</p>'
+                                    f'<p>Library User First Name: {loanee_first_name}</p>'
+                                f'</div>'
+                            f'</div>'
+                        f'</div>',
+                        unsafe_allow_html=True
+                    )
+                    st.text('')
+                else: # Livro
+                    book_name = Book(loan.id_book).get_book_by_isbn().title
+                    st.markdown(
+                        f'<div style="display: flex; align-items: center; padding: 10px; border-radius: 5px; background-color: {background_color}; color: black">'
+                            f'<div style="flex: 1; padding-right: 10px;">'
+                                f'<h2 style="color: black">ID: {loan.id}</h2>'
+                                f'<div style="display: flex; flex-direction: column; flex-wrap: wrap; margin-left: 20px;">'
+                                    f'<p>Status: {loan.status}</p>'
+                                    f'<p>Loan Date: {loan.loan_date}</p>'
+                                    f'<p>Expected Return Date: {loan.expected_return_date}</p>'
+                                    f'<p>Book ID: {loan.id_book}</p>'
+                                    f'<p>Book Name: {book_name}</p>'
+                                    f'<p>Library User ID: {loan.id_user}</p>'
+                                    f'<p>Library User First Name: {loanee_first_name}</p>'
+                                f'</div>'
+                            f'</div>'
+                        f'</div>',
+                        unsafe_allow_html=True
+                    )
+                    st.text('')
  
