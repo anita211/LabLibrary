9e8082ac83b988cf814184fe1d8fdeb3ea85cc47
Victor André <victorandre957@gmail.com>
2023-12-13 20:08:20 -0300

    save logged_user None

--- src/globals.py
+++ src/globals.py
@@ -1,1 +1,1 @@
-logged_user = {'id': 2, 'username': 'nome456', 'first_name': 'Nina', 'last_name': 'Oliveira', 'password': 'c33367701511b4f6020ec61ded352059', 'user_photo_url': None, 'role': 'MEMBER'}
+logged_user = {'role': None}
