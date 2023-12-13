4a88ddfcb3f538032bacb586ee702872079ac6ab
Victor André <victorandre957@gmail.com>
2023-12-13 16:23:09 -0300

    fix login

--- src/Home.py
+++ src/Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from api.users import User
 
 with open('src/globals.py', 'r') as file:
     exec(file.read())
 
-with open('src/globals.py', 'w') as file:
-    file.write(f"logged_user = {None}")
-
-User(1, 'nome123', 'Pafuncio', 'Pinto', '123456', 'ADMIN').authenticate_user()
-
 st.title('Book Manager')
+
+st.text('')
+
+if st.button("Logout", use_container_width=True):
+    with open('src/globals.py', 'w') as file:
+        file.write(f"logged_user = {None}")
 
--- src/pages/Login.py
+++ src/pages/Login.py
@@ -1,20 +1,28 @@
 import streamlit as st
 from api.users import User
+from globals import logged_user
 
-st.header('Login to Your Account')
+with open('src/globals.py', 'r') as file:
+    exec(file.read())
 
-# Get username and password from user input
-username = st.text_input('Username')
-password = st.text_input('Password', type='password')
+if logged_user is None:
+    st.header('Login to Your Account')
 
-if st.button('Login'):
-    # Check if the user exists and authenticate
-    user = User(username=username, password=password).authenticate_user()
+    # Get username and password from user input
+    username = st.text_input('Username')
+    password = st.text_input('Password', type='password')
 
-    if user:
-        st.success(f'Welcome, {user.first_name} {user.last_name}!')
-        st.write(f'User ID: {user.id}')
-        st.write(f'Role: {user.role}')
-        st.write(f'User Photo URL: {user.user_photo_url}')
-    else:
-        st.error('Invalid username or password. Please try again.')
+    if st.button('Login'):
+        # Check if the user exists and authenticate
+        user = User(username=username, password=password).authenticate_user()
+
+        if user:
+            st.success(f'Welcome, {user.first_name} {user.last_name}!')
+            st.write(f'User ID: {user.id}')
+            st.write(f'Role: {user.role}')
+            st.write(f'User Photo URL: {user.user_photo_url}')
+        else:
+            st.error('Invalid username or password. Please try again.')
+
+else:
+    st.header('You are already logged in')
